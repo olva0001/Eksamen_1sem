cbbfc1b56c4d01efc1cb16fd0fe4a217ee4bb045
Oliver <olva0001@stud.kea.dk>
2024-06-07 01:14:50 +0200

    .

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const rows = document.querySelectorAll('.row_1, .row_2, .row_3, .row_4, .row_5');
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('in-view');
-          observer.unobserve(entry.target); // Stop observing the element after adding the class
-        }
-      });
-    }, { threshold: 0.1 });
-  
-    rows.forEach(row => {
-      observer.observe(row);
+  const rows = document.querySelectorAll('.row_1, .row_2, .row_3, .row_4, .row_5');
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+        observer.unobserve(entry.target); // Stop observing the element after adding the class
+      }
     });
+  }, { threshold: 0.1 });
+
+  rows.forEach(row => {
+    observer.observe(row);
   });
+});
