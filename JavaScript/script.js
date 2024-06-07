a272cf780aa15ed95f32f15b4a8cda63a5f0bc2b
Oliver <olva0001@stud.kea.dk>
2024-06-07 10:03:35 +0200

    oprettet css dokumenter til mobile queries

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,13 @@
+
+/* Forside tema præsentationer */
+
 document.addEventListener('DOMContentLoaded', function() {
   const rows = document.querySelectorAll('.row_1, .row_2, .row_3, .row_4, .row_5');
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add('in-view');
-        observer.unobserve(entry.target); // Stop observing the element after adding the class
+        observer.unobserve(entry.target);
       }
     });
   }, { threshold: 0.1 });
@@ -12,4 +15,21 @@ document.addEventListener('DOMContentLoaded', function() {
   rows.forEach(row => {
     observer.observe(row);
   });
+});
+
+/* Lottiefile */
+
+document.addEventListener('scroll', function() {
+  const lottieContainer = document.querySelector('.lottie_container');
+  const lottiePlayer = document.querySelector('lottie-player');
+  const containerHeight = lottieContainer.offsetHeight;
+  const scrollY = window.scrollY;
+  const windowHeight = window.innerHeight;
+
+  if (scrollY < containerHeight) {
+    const opacity = 1 - (scrollY / (containerHeight / 2)); // Adjust the denominator for a smoother fade
+    lottiePlayer.style.opacity = opacity;
+  } else {
+    lottiePlayer.style.opacity = 0;
+  }
 });
